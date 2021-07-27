bcd17c37079e16067019e2dcbb6b54d5d7935c09
Maddy <madalyn.redpath@gmail.com>
2021-07-27 18:31:44 +1000

    Initial setup for DateDifferenceProject including unit tests

---
+++ DateDifferenceProject/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using static System.Console;
+
+namespace DateDifferenceProject
+{
+    public class DateDifference
+    {
+        //defining parameters of a date
+        public class MyDate
+        {
+            public int day, month, year;
+
+            public MyDate(int day, int month, int year)
+            {
+                this.day = day;
+                this.month = month;
+                this.year = year;
+            }
+        }
+
+        //store number of days in each month
+        //e.g Jan = 31 days, Feb = 28 days ... Dec = 31 days
+        public static int[] daysInMonths = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+        //count the number of leap years
+        public static int countLeapYears(MyDate day)
+        {
+            int years = day.year;
+
+            //check if input date is a leap year
+            if (day.month <= 2)
+            {
+                years--;
+            }
+
+            //A leap year occurs if it is divisible by 4 or 400, and not 100
+            //e.g. 1900 = not a leap year, 2000 = a leap year
+            return years / 4 - years / 100 + years / 400;
+
+        }
+
+        //calculate the difference (days) between the two inputted dates 
+        //by calculating the number of days from 01/01/0000 for each inputted date.
+
+        public static int calcDaysDifference(MyDate date1, MyDate date2)
+        {
+            //Count the days before date1
+
+            //count using years and day
+            int count1 = date1.year * 365 + date1.day;
+
+            //add days to months for inputted date
+            for (int i = 0; i < date1.month - 1; i++)
+            {
+                count1 += daysInMonths[i];
+            }
+
+            //for a leap year, add a day
+            count1 += countLeapYears(date1);
+
+
+            //repeat above for date2
+            int count2 = date2.year * 365 + date2.day;
+            for (int i = 0; i < date2.month - 1; i++)
+            {
+                count2 += daysInMonths[i];
+            }
+            count2 += countLeapYears(date2);
+
+            return (count2 - count1);
+        }
+        public static int getDaysInMonth(int month, int year)
+        {
+            if (month != 2)
+            {
+                return daysInMonths[month - 1];
+            }
+
+            //special case for February
+            //A leap year occurs if it is divisible by 4 or 400, and not 100
+            else
+            {
+                if ((year % 4 == 0 && year % 400 == 0) || (year % 4 == 0 && !(year % 100 == 0)))
+                {
+                    return 29;
+                }
+
+                else
+                {
+                    return 28;
+                }
+            }
+        }
+
+        public static MyDate parseInputandValidate(string inputDate)
+        {
+            //to split the user input into day/month/year
+            int day, month, year;
+            bool daySuccess = false, monthSuccess = false, yearSuccess = false;
+
+            try
+            {
+                String[] arrDateSplit = inputDate.Split(' ');
+                daySuccess = int.TryParse(arrDateSplit[0], out day);
+                monthSuccess = int.TryParse(arrDateSplit[1], out month);
+                yearSuccess = int.TryParse(arrDateSplit[2], out year);
+            }
+
+            catch
+            {
+                WriteLine("Invalid date input. Please try again");
+                return null;
+            }
+
+            bool invalidDateNumbers = daySuccess == false || monthSuccess == false || yearSuccess == false;
+            
+            //add constraints for date input
+            if (invalidDateNumbers ||
+                month < 1 || month > 12 ||
+                day < 1 || day > getDaysInMonth(month, year) ||
+                year < 1900 || year > 2010)
+            {
+                WriteLine("Invalid date input. Please try again");
+                return null;
+            }
+
+            var result = new MyDate(day, month, year);
+
+            return result;
+        }
+
+        public static void Main(string[] args)
+        {
+            MyDate date1 = null;
+            MyDate date2 = null;
+
+            string firstDate = string.Empty;
+            string secondDate = string.Empty;
+
+            //first date input
+
+            while (date1 == null)
+            {
+                WriteLine("Enter the earliest date in the format DD MM YYYY: ");
+                firstDate = ReadLine();
+                date1 = parseInputandValidate(firstDate);
+            }
+
+            //second date input
+            while (date2 == null)
+            {
+                WriteLine("Enter the latest date in the format DD MM YYYY: ");
+                secondDate = ReadLine();
+                date2 = parseInputandValidate(secondDate);
+                if (date2 == null)
+                {
+                    continue;
+                }
+                //ensure earliest date entered first
+                if (calcDaysDifference(date1, date2) < 0)
+                {
+                    WriteLine("Earliest date not entered first. Try again");
+                    date2 = null;
+                }
+            }
+
+            WriteLine($"{firstDate}, {secondDate}, " + calcDaysDifference(date1, date2) + " days");
+            ReadLine();
+
+        }
+    }
+}
+
+
---
+++ DateDifferenceProjectTests/DateDifferenceTests.cs
@@ -0,0 +1,227 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DateDifferenceProject;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static DateDifferenceProject.DateDifference;
+
+namespace DateDifferenceProject.Tests
+{
+    [TestClass()]
+    public class DateDifferenceTests
+    {
+        private void compareDates(MyDate expected, MyDate result)
+        {
+            Assert.AreEqual(expected.day, result.day);
+            Assert.AreEqual(expected.month, result.month);
+            Assert.AreEqual(expected.year, result.year);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnNullBadInput()
+        {
+            // Arrange
+            string inputDate = "a a a";
+            MyDate expected = null;
+
+            // Act
+            MyDate result = DateDifference.parseInputandValidate(inputDate);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnNullInvalidSpacing()
+        {
+            // Arrange
+            string inputDate = "20/02/1998";
+            MyDate expected = null;
+
+            // Act
+            MyDate result = DateDifference.parseInputandValidate(inputDate);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnNullInvalidMonth()
+        {
+            // Arrange
+            string inputDate = "20 13 1998";
+            MyDate expected = null;
+
+            // Act
+            MyDate result = DateDifference.parseInputandValidate(inputDate);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnNullOutsideLowerYearBounds()
+        {
+            // Arrange
+            string inputDate = "01 01 1800";
+            MyDate expected = null;
+
+            // Act
+            MyDate result = DateDifference.parseInputandValidate(inputDate);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnNullIfInvalidDay()
+        {
+            // Arrange
+            string inputDate = "99 01 1992";
+            MyDate expected = null;
+
+            // Act
+            MyDate result = DateDifference.parseInputandValidate(inputDate);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnDateOnLowerBound()
+        {
+            // Arrange
+            //lower date range
+            string inputDate = "01 01 1900";
+            MyDate expected = new MyDate(1, 1, 1900);
+
+            // Act
+            MyDate result = DateDifference.parseInputandValidate(inputDate);
+
+            // Assert
+            compareDates(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldReturnDateOnUpperBound()
+        {
+            // Arrange
+            //upper date range
+            string inputDate = "31 12 2010";
+            MyDate expected = new MyDate(31, 12, 2010);
+
+            // Act
+            MyDate result = DateDifference.parseInputandValidate(inputDate);
+
+            // Assert
+            compareDates(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldCalcDaysBetweenTwoDates()
+        {
+            // Arrange
+            //date1, date2
+            MyDate date1 = new MyDate(04, 02, 1992);
+            MyDate date2 = new MyDate(04, 02, 1993);
+            int expected = 366;
+
+            // Act
+            var result = DateDifference.calcDaysDifference(date1, date2);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod()]
+        public void ShouldCalcDaysLatestDateFirst()
+        {
+            // Arrange
+            //date1, date2
+            MyDate date1 = new MyDate(04, 02, 1993);
+            MyDate date2 = new MyDate(04, 02, 1992);
+            int expected = -366;
+
+            // Act
+            var result = DateDifference.calcDaysDifference(date1, date2);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod()]
+        public void ShouldGetDaysInMonth()
+        {
+            // Arrange
+            int inputMonth = 1;
+            int inputYear = 1991;
+            int expected = 31;
+
+            // Act
+            var result = DateDifference.getDaysInMonth(inputMonth, inputYear);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldGetDaysInMonthFebLeap()
+        {
+            // Arrange
+            int inputMonth = 2;
+            int inputYear = 1992;
+            int expected = 29;
+
+            // Act
+            var result = DateDifference.getDaysInMonth(inputMonth, inputYear);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldGetDaysInMonthFebNotLeap()
+        {
+            // Arrange
+            int inputMonth = 2;
+            int inputYear = 1900;
+            int expected = 28;
+
+            // Act
+            var result = DateDifference.getDaysInMonth(inputMonth, inputYear);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldCountCorrectlyOnLeapYears()
+        {
+            // Arrange
+            MyDate date = new MyDate(04, 02, 1992);
+            int expected = 482;
+
+            // Act
+            var result = DateDifference.countLeapYears(date);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        public void ShouldCountCorrectlyOnNotLeapYears()
+        {
+            // Arrange
+            MyDate date = new MyDate(27, 05, 2005);
+            int expected = 486;
+
+            // Act
+            var result = DateDifference.countLeapYears(date);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
